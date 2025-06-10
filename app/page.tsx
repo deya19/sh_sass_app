e63eeb090f37797ccdffc7d6f80467f2e9c142c8
deya19 <deyasaleh2@gmail.com>
2025-06-10 10:58:21 +0300

    saas app

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button'
+
+
+export default function page() {
+  return (
+    <div>
+      <h1 className='text-3xl font-bold'>Hello World</h1>
+      <Button>Click me</Button>
+    </div>
+  )
+}
